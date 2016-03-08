ea6943d04874cc6d772de7398732655dfc8ca92f
amatt13 <anders.matthiassen@gmail.com>
2016-03-08 15:43:37 +0100

    Test methods have been set up. All of them fails

--- rest_client/src/CiscoDataTransfer/CiscoPuller.java
+++ rest_client/src/CiscoDataTransfer/CiscoPuller.java
@@ -2,8 +2,11 @@ package CiscoDataTransfer;
 
 
 import AllClient.AllClient;
+import com.sun.prism.paint.Stop;
 
 import java.io.IOException;
+import java.util.Objects;
+
 import static CiscoDataTransfer.CiscoClient.httpGet;
 
 
@@ -13,40 +16,73 @@ import static CiscoDataTransfer.CiscoClient.httpGet;
 public class CiscoPuller extends Thread {
 
     @Override public void run() {
-        long time;
-        while(true) {
-            time = System.currentTimeMillis();
-            String data = GetCiscoData();
+        if(!ContinuesPuller()) {
+            System.out.println("It was not possible to pull data from the Cisco server\n" +
+                    "Is the server running?");
+        }
+    }
+
+    // Pulls data from the Cisco server. If numberOfPulls is null, the server will pull until stopped
+    protected static Boolean ContinuesPuller(int numberOfPulls)  {
+         Boolean pulledUntilStop = true;
+        for (; numberOfPulls > 0; numberOfPulls--) {
+            pulledUntilStop = ContinuesPullerHelper();
+            if (!pulledUntilStop) return  false;
+        }
+        return pulledUntilStop;
+    }
+
+    protected static Boolean ContinuesPuller()  {
+         try {
+             while (ContinuesPullerHelper());
+         }
+         catch (Exception e){
+             e.printStackTrace();
+             return false;
+         }
+        return false;
+    }
+
+    private static Boolean ContinuesPullerHelper (){
+        long time = System.currentTimeMillis();
+        // Pull data via httpGEt
+        String password = "works",
+                user = "test",
+                target = "http://172.26.120.61:8080/api/contextaware/v1/location/clients",
+                data = GetCiscoData(target, user, password);
+
+        if (!Objects.equals(data, "null")) {
             System.out.println("Pulled Data at " + System.currentTimeMillis());
             // >>Convert to a Java Class?<<
             //AllClient CiscoDataTransfer = CiscoClient.ReadJsonToClientList(data);
             // >>Convert to a more readable string?<<
             //data = CiscoDataTransfer.getLocations().getEntries().toString();
-
             SendToDB(data);
-
             try {
                 sleep(time + 300000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
-                break;
+                return false;
             }
         }
+        else {
+            return false;
+        }
+        return true;
     }
 
-    public String GetCiscoData() {
+    protected static String GetCiscoData(String target, String user, String password) {
         String data = "null";
         try {
             System.out.println(
-                    data = httpGet("http://172.26.120.61:8080/api/contextaware/v1/location/clients",
-                    "test", "works"));
+                    data = httpGet(target, user, password));
         } catch (IOException e) {
-            e.printStackTrace();
+            //e.printStackTrace();
         }
         return data;
     }
 
-    private Boolean SendToDB (String data) {
+    protected static Boolean SendToDB (String data) {
         try {
             // some send function
             return true;
@@ -56,7 +92,7 @@ public class CiscoPuller extends Thread {
         }
     }
 
-    private Boolean SendToDB (AllClient data) {
+    protected static Boolean SendToDB (AllClient data) {
         try {
             // some send function
             return true;
--- rest_client/src/CiscoDataTransfer/CiscoPullerTest.java
+++ rest_client/src/CiscoDataTransfer/CiscoPullerTest.java
@@ -1,6 +1,6 @@
 package CiscoDataTransfer;
 
-import org.junit.After;;
+import org.junit.After;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -11,9 +11,16 @@ import static org.junit.Assert.*;
  */
 public class CiscoPullerTest {
 
+    public static String password,
+            user ,
+            target;
+
     @BeforeClass
     public static void setUp() throws Exception {
         System.out.printf("CiscoPuller tests initiated... \n\n");
+        password = "works";
+        user = "test";
+        target = "http://127.0.0.1:8080/api/contextaware/v1/location/clients";
     }
 
     @After
@@ -23,11 +30,48 @@ public class CiscoPullerTest {
 
     @Test
     public void testRun() throws Exception {
+        // Intentionally not tested
+        // The method is trivial
+        // CiscoDataTransfer.CiscoPuller.run()
+    }
 
+    @Test
+    public void testContinuesPuller1() throws Exception {
+        // This should be tested on a local server
+        // It is currently tested on the online server
+        assertEquals("Testing 'ContinuesPuller' with a positive integer", true,
+                CiscoPuller.ContinuesPuller(10));
+        assertEquals("Testing 'ContinuesPuller' with a negative integer", true,
+                CiscoPuller.ContinuesPuller(-1));
+        assertEquals("Testing 'ContinuesPuller' with 0 value integer", true,
+                CiscoPuller.ContinuesPuller(0));
+    }
+
+    @Test
+    public void testContinuesPuller2() throws Exception {
+        // This should be tested on a local server
+        // It is currently tested on the online server
+        assertEquals("Testing 'ContinuesPuller' with 0 arguments", true,
+                CiscoPuller.ContinuesPuller());
     }
 
     @Test
     public void testGetCiscoData() throws Exception {
+        // This should be tested on a local server
+        // The server is not started yet
+        assertEquals("Testing GetCiscoData", "expected result",
+                CiscoPuller.GetCiscoData(target, user, password));
+    }
 
+    @Test
+    public void testSendToDB1() throws  Exception {
+        // Actual function is not implemente
+        // CiscoDataTransfer.CiscoPuller.SendToDB(java.lang.String)
+    }
+
+    @Test
+    public void testSendToDB2() throws  Exception {
+        // Actual function is not implemented
+        // CiscoDataTransfer.CiscoPuller.SendToDB(AllClient.AllClient)
     }
 }
